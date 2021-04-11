f295c7fbfbd7ffcd3d596825312e19062591e854
Hyunsu Kim <hyunsujk@gmail.com>
2021-04-10 19:57:53 -0500

    ran ng generate component trivia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TriviaComponent } from './trivia/trivia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TriviaComponent
   ],
   imports: [
     BrowserModule
