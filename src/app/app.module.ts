de57ff769b9074f9d518ecf82cc59baf1b30b24e
Hyunsu Kim <hyunsujk@gmail.com>
2021-04-10 20:09:27 -0500

    imported router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Route, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TriviaComponent } from './trivia/trivia.component';
 
+const routes: Route[] = [];
 @NgModule({
-  declarations: [
-    AppComponent,
-    TriviaComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TriviaComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
