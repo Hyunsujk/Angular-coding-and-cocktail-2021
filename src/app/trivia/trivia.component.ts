dd261382380d67ea26caf85aa7f2a2a071faeb12
Hyunsu Kim <hyunsujk@gmail.com>
2021-06-12 20:48:20 -0500

    use questions from api

--- src/app/trivia/trivia.component.ts
+++ src/app/trivia/trivia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Question } from '../question';
 
 @Component({
@@ -7,50 +8,7 @@ import { Question } from '../question';
   styleUrls: ['./trivia.component.css'],
 })
 export class TriviaComponent implements OnInit {
-  public questions: Question[] = [
-    {
-      text: 'Florence Nightingale became known as "the Lady With the Lamp" during which war?',
-      answers: [
-        {
-          correct: false,
-          text: 'American Civil War',
-        },
-        {
-          correct: false,
-          text: 'World War I',
-        },
-        {
-          correct: true,
-          text: 'Crimean War',
-        },
-        {
-          correct: false,
-          text: 'World War II',
-        },
-      ],
-    },
-    {
-      text: 'In a quarter-mile race, which animal can be expected to win?',
-      answers: [
-        {
-          correct: false,
-          text: 'Lion',
-        },
-        {
-          correct: true,
-          text: 'Pronghorn Antelope',
-        },
-        {
-          correct: false,
-          text: 'Quarter Horse',
-        },
-        {
-          correct: false,
-          text: 'Giraffe',
-        },
-      ],
-    },
-  ];
+  public questions$: Observable<Question[]>;
 
   public correctAnswers = 0;
 
